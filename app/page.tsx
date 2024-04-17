28e06d8034199b04c7a819a8c0a497c9b63aea87
Dev Patel <144943599+devpatel2404@users.noreply.github.com>
2024-04-17 13:35:30 -0400

    Making The Initial Home Page Structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,93 @@
+'use client'
 import Image from "next/image";
+import {useEffect, useState} from "react";
+import {element} from "prop-types";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    let [scheme, setScheme] = useState('');
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const changeLight = () => {
+        document.getElementById("Background").style.backgroundColor = "white";
+        document.getElementById("Background").style.color = 'black';
+        setScheme("Light");
+    };
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    const changeDark = () => {
+        document.getElementById("Background").style.backgroundColor = "black";
+        document.getElementById("Background").style.color = 'white';
+        setScheme("Dark");
+    };
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    const Home = () => {
+        document.getElementById("AboutMe").hidden = true;
+        document.getElementById("Projects").hidden = true;
+        document.getElementById("Contact").hidden = true;
+        document.getElementById("Home").hidden = false;
+    }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+    const AboutMe = () => {
+        document.getElementById("Projects").hidden = true;
+        document.getElementById("Contact").hidden = true;
+        document.getElementById("Home").hidden = true;
+        document.getElementById("AboutMe").hidden = false;
+    }
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    const Projects = () => {
+        document.getElementById("AboutMe").hidden = true;
+        document.getElementById("Contact").hidden = true;
+        document.getElementById("Home").hidden = true;
+        document.getElementById("Projects").hidden = false;
+    }
+
+    const Contact = () => {
+        document.getElementById("AboutMe").hidden = true;
+        document.getElementById("Projects").hidden = true;
+        document.getElementById("Home").hidden = true;
+        document.getElementById("Contact").hidden = false;
+    }
+
+    return (
+        <main className={`h-screen`} id={"Background"}>
+            <div id={"Page"}></div>
+            <div className={"Container"}>
+                <div className={"flex justify-end pt-2 Options"}>
+                    <input type={"radio"} id={"Light"} name="Color_Schemes" className={"mr-0.5"} onClick={changeLight}
+                           defaultChecked={scheme === "Light"}/>
+                    <h1 className={"mr-1.5"}>Light</h1>
+                    <input type={"radio"} id={"Dark"} name="Color_Schemes" onClick={changeDark} className={"mr-0.5"}/>
+                    <h1 className={"mr-1.5"}>Dark</h1>
+                    <input type={"radio"} id={"Times"} name={"Fonts"} value={"Arial"} defaultChecked={true}/>
+                    <h1 className={"mr-1.5"}>Arial</h1>
+                    <input type={"radio"} id={"Times"} name={"Fonts"} value={"Helvetica"}/>
+                    <h1 className={"mr-1.5"}>Helvetica</h1>
+                    <input type={"radio"} id={"Sans"} name={"Fonts"} value={"Sans-Serif"}/><h1>Mono</h1>
+                </div>
+                <div
+                    className={`font-sans text-center border-2 h-full ${scheme === "Light" ? 'border-blue-700' : "border-cyan-400"}`}
+                    style={{height: '95vh'}}>
+                    <h1 className={"mt-2 mr-2 text-4xl"}>Dev Patel</h1>
+                    <h1 className={"mt-0.5 mr-2 text-lg"}>Full Stack Engineer</h1>
+                    <div className={"flex justify-center"} id={"NavBar"}>
+                        <button onClick={Home} type={"button"}><h1 className={`mr-3`}>Home</h1></button>
+                        <button onClick={AboutMe} type={"button"}><h1 className={"mr-3"}>About Me</h1></button>
+                        <button onClick={Projects} type={"button"}><h1 className={"mr-3"}>Projects</h1></button>
+                        <button onClick={Contact} type={"button"}><h1 className={""}>Contact</h1></button>
+                    </div>
+                    <div id={"Home"} hidden={false}>
+                        <h1>Home</h1>
+                    </div>
+                    <div id={"AboutMe"} hidden={true}>
+                        <h1>About Me</h1>
+                    </div>
+                    <div id={"Projects"} hidden={true}>
+                        <h1>Projects</h1>
+                    </div>
+                    <div id={"Contact"} hidden={true}>
+                        <h1>Contact</h1>
+                    </div>
+                </div>
+
+            </div>
+        </main>
+    );
 }
