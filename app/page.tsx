fcbfb5d2561d4c468c8491166ed0ca214e813929
Dev Patel <144943599+devpatel2404@users.noreply.github.com>
2024-08-01 21:57:40 -0400

    Fixing Page Styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
 'use client'
 import Link from "next/link";
-import {useState, useEffect} from "react";
+import {useState, useEffect, ChangeEvent} from "react";
+import {green} from "next/dist/lib/picocolors";
 
 export default function Home() {
     const [theme, setTheme] = useState(window.matchMedia(`(prefers-color-scheme: dark)`).matches ? 'dark' : 'light');
     const [font, setFont] = useState('Futura');
     const [tempTheme, setTempTheme] = useState(window.matchMedia(`(prefers-color-scheme: dark)`).matches ? 'dark' : 'light');
     const [tempFont, setTempFont] = useState('Futura');
+    const [activeSection, setActiveSection] = useState('Home');
 
     useEffect(() => {
         const mediaQuery = window.matchMedia(`(prefers-color-scheme: dark)`);
         setTheme(mediaQuery.matches ? 'dark' : 'light');
 
-        const handleChange = (e) => {
+        const handleChange = (e: MediaQueryListEvent) => {
             setTheme(e.matches ? 'dark' : 'light');
         }
 
@@ -23,28 +25,32 @@ export default function Home() {
         }
     }, []);
 
-    const handleThemeChange = (e) => {
+    const handleThemeChange = (e: ChangeEvent<HTMLInputElement>) => {
       setTheme(e.target.value);
     };
 
-    const handleFontChange = (e) => {
+    const handleFontChange = (e: ChangeEvent<HTMLInputElement>) => {
         setFont(e.target.value);
     }
 
-    const themeEnter = (e) => {
-        setTempTheme(e.target.value);
+    const themeEnter = (newTheme: string) => {
+        setTempTheme(newTheme);
     }
 
-    const themeExit = (e) => {
-        setTempTheme('');
+    const themeExit = (q: string) => {
+        setTempTheme(q);
     }
 
-    const fontEnter = (e) => {
-        setTempFont(e.target.value);
+    const fontEnter = (newFont: string) => {
+        setTempFont(newFont);
     }
 
-    const fontExit = (e) => {
-        setTempFont('');
+    const fontExit = (q:string) => {
+        setTempFont(q);
+    }
+
+    const showSection = (section:string) => {
+        setActiveSection(section);
     }
 
     const appliedTheme = tempTheme || theme;
@@ -57,20 +63,24 @@ export default function Home() {
                     <h1 className={"Title"}>Dev Patel</h1>
                     <h1 className={"Role"}>Full Stack Engineer</h1>
                     <div className={"flex justify-center"} id={"NavBar"}>
-                        <button type={"button"}><h1 className={`mr-4`}>Home</h1></button>
-                        <button type={"button"}><h1 className={"mr-4"}>About Me</h1></button>
-                        <button type={"button"}><h1 className={"mr-4"}>Projects</h1></button>
-                        <button type={"button"}><h1 className={""}>Contact</h1></button>
+                        <button type={"button"} onClick={() => showSection("Home")}><h1 className={`mr-4`}>
+                            Home</h1></button>
+                        <button type={"button"} onClick={() => showSection("About")}><h1 className={"mr-4"}>
+                            About</h1></button>
+                        <button type={"button"} onClick={() => showSection("Projects")}><h1 className={"mr-4"}>
+                            Projects</h1></button>
+                        <button type={"button"} onClick={() => showSection("Contact")}><h1 className={""}>
+                            Contact</h1></button>
                     </div>
-                    <div id={"Home"} hidden={false}></div>
-                    <div id={"AboutMe"} hidden={true}>
+                    <div id={"Home"} style={{display: activeSection == "Home" ? "initial" : "none"}}></div>
+                    <div id={"AboutMe"} style={{display: activeSection == "About" ? "block" : "none"}}>
                         <h1>About Me</h1>
                     </div>
-                    <div id={"Projects"} hidden={true}>
+                    <div id={"Projects"} style={{display: activeSection == "Projects" ? "initial" : "none"}}>
                         <h1>Projects</h1>
                         <Link href={"https://vitality-hub-iota.vercel.app"} target={"_blank"}>Hello</Link>
                     </div>
-                    <div id={"Contact"} hidden={true}>
+                    <div id={"Contact"} style={{display: activeSection == "Contact" ? "initial" : "none"}}>
                         <h1>devpatel4528@gmail.com</h1>
                         <h1>Github</h1>
                         <h1>LinkedIn</h1>
@@ -79,20 +89,21 @@ export default function Home() {
             </div>
             <div className={"Options"}>
                 <input type={"radio"} className={"Radio Light"} value={'light'} name={'theme'} checked={theme === 'light'}
-                       onChange={handleThemeChange} onMouseEnter={themeEnter} onMouseLeave={themeExit} />
+                       onChange={handleThemeChange} onMouseEnter={() => themeEnter("light")} onMouseLeave={() => themeExit('')} />
                 <h1 className={"Color"}>Light</h1>
                 <input type={"radio"} className={"Radio Dark"} value={'dark'} name={'theme'} checked={theme === 'dark'}
-                       onChange={handleThemeChange} onMouseEnter={themeEnter} onMouseLeave={themeExit}/>
+                       onChange={handleThemeChange} onMouseEnter={() => themeEnter("dark")} onMouseLeave={() => themeExit('')}/>
+
                 <h1 className={"Color"}>Dark</h1>
 
-                <input type={"radio"} className={"Radio Arial"} defaultChecked={true} value={"Arial"} name={'Font'}
-                       onChange={handleFontChange} onMouseEnter={fontEnter} onMouseLeave={fontExit}/>
+                <input type={"radio"} className={"Radio Arial"} defaultChecked={true} value={"Futura"} name={'Font'}
+                       onChange={handleFontChange} onMouseEnter={() => fontEnter("Futura")} onMouseLeave={() => fontExit('')}/>
                 <h1 className={"Futura"}>Futura</h1>
-                <input type={"radio"} className={"Radio Comic"} value={"Comic"} name={'Font'}
-                       onChange={handleFontChange} onMouseEnter={fontEnter} onMouseLeave={fontExit}/>
+                <input type={"radio"} className={"Radio Comic"} value={"Baskerville"} name={'Font'}
+                       onChange={handleFontChange} onMouseEnter={() => fontEnter("Baskerville")} onMouseLeave={() => fontExit('')}/>
                 <h1 className={"Baskerville"}>Baskerville</h1>
                 <input type={"radio"} className={"Radio Mono"} name={"Font"} value={"Mono"}
-                       onChange={handleFontChange} onMouseEnter={fontEnter} onMouseLeave={fontExit}/>
+                       onChange={handleFontChange} onMouseEnter={() => fontEnter("Mono")} onMouseLeave={() => fontExit('')}/>
                 <h1 className={"Mono"}>Mono</h1>
             </div>
         </main>
